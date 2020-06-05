7cd14e8980f0b7de1f69c2f697edb06621d7687c
BenDunjay <bendunjay@outlook.com>
2020-06-05 16:52:51 +0100

    end of day backup with availabilities working

---
+++ app/controllers/api/v1/availabilities_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::AvailabilitiesController < ApplicationController
+  def index
+    availability = Availability.all
+    render json: availabilities, include: [:artists]
+  end
+
+  def create_availability
+    @availability = Availability.create(availability_params)
+    if @availability.valid?
+      render json: { availability: AvailabilitySerializer.new(@availability) }, status: :created
+    else
+      render json: { error: "failed to create availability" }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def availability_params
+    params.require(:availability).permit(:date, :artist_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :availabilities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :artists, only: [:create]
       post "/artist_login", to: "auth#artist_login"
       get "/profile", to: "artists#profile"
       patch "/edit_profile", to: "artists#edit_profile"
+      post "/create_availability", to: "availabilities#create_availability"
     end
   end
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
