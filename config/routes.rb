aa0d1b00dd8730d1a044d78cd26ad2eeeea9fc8b
BenDunjay <bendunjay@outlook.com>
2020-11-25 15:16:44 +0000

    removed syntactic sugar scope, to make it more obvious in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,24 @@ Rails.application.routes.draw do
       resources :venues, only: [:create]
       resources :artists, only: [:create]
 
-      scope controller: :artists do
+      controller :artists do
         get "/profile/:name" => :profile
         patch "/edit_profile" => :edit_profile
         get "/all_artists" => :all_artists
       end
 
-      scope controller: :auth do
+      controller :auth do
         get "/authorize" => :refresh
         post "/artist_login" => :artist_login
         post "/venue_login" => :venue_login
       end
 
-      scope controller: :availabilities do
+      controller :availabilities do
         post "/create_availability" => :create_availability
         get "/get_availability/:id" => :get_availability
       end
 
-      scope controller: :requests do
+      controller :requests do
         post "/create_request" => :create_request
         patch "/accept_or_reject/:id" => :accept_or_reject
         get "/all_bookings" => :all_bookings
