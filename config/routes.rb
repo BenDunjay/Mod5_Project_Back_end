1c6c393c62911ada0c845b734576cdc6db6330d8
BenDunjay <bendunjay@outlook.com>
2020-11-25 15:14:40 +0000

    tidied up routes file using scope method

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,29 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :venues, only: [:create]
       resources :artists, only: [:create]
-      get "/authorize", to: "auth#refresh"
-      post "/artist_login", to: "auth#artist_login"
-      get "/profile/:name", to: "artists#profile"
-      patch "/edit_profile", to: "artists#edit_profile"
-      post "/create_availability", to: "availabilities#create_availability"
-      post "/venue_login", to: "auth#venue_login"
-      get "/all_artists", to: "artists#all_artists"
-      get "/get_availability/:id", to: "availabilities#get_availability"
-      post "/create_request", to: "requests#create_request"
-      patch "/accept_or_reject/:id", to: "requests#accept_or_reject"
-      get "/all_bookings", to: "requests#all_bookings"
+
+      scope controller: :artists do
+        get "/profile/:name" => :profile
+        patch "/edit_profile" => :edit_profile
+        get "/all_artists" => :all_artists
+      end
+
+      scope controller: :auth do
+        get "/authorize" => :refresh
+        post "/artist_login" => :artist_login
+        post "/venue_login" => :venue_login
+      end
+
+      scope controller: :availabilities do
+        post "/create_availability" => :create_availability
+        get "/get_availability/:id" => :get_availability
+      end
+
+      scope controller: :requests do
+        post "/create_request" => :create_request
+        patch "/accept_or_reject/:id" => :accept_or_reject
+        get "/all_bookings" => :all_bookings
+      end
     end
   end
 end
